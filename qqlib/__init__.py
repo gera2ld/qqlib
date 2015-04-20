49f636a2e01bf46885b707e1383c70640c0036ed
Gerald <gera2ld@163.com>
2015-04-20 12:28:18 +0800

    cleaned code
    Signed-off-by: Gerald <gera2ld@163.com>

--- qqlib/__init__.py
+++ qqlib/__init__.py
@@ -1,23 +1,36 @@
 #!python
 # coding=utf-8
-import os,hashlib,re,tempfile,rsa,binascii,base64,requests
+'''
+QQ Login module
+Maintainer: Gerald <gera2ld@163.com>
+Last change: 2015 Apr 20
+'''
+import os, hashlib, re, tempfile, binascii, base64
+import rsa, requests
 from . import tea
 __all__ = ['QQ', 'LogInError']
 
 class LogInError(Exception): pass
 
 class QQ:
+	'''
+	>>> qq = QQ(12345678, 'password')
+	>>> qq.login()
+	>>> qq.sayHi()
+	'''
 	appid=501004106
 	action='0-21-1083992'
 	proxyurl='http://w.qq.com/proxy.html'
 	checkurl='https://ssl.ptlogin2.qq.com/check'
 	imgurl='http://captcha.qq.com/getimage'
 	loginurl='https://ssl.ptlogin2.qq.com/login'
-	def __init__(self,user,pwd):
+
+	def __init__(self, user, pwd):
 		super().__init__()
-		self.user=user
-		self.pwd=pwd
-		self.cookies=None
+		self.user = user
+		self.pwd = pwd
+		self.cookies = None
+
 	def fetch(self, url, data=None, **kw):
 		if data is None:
 			func = requests.get
@@ -28,8 +41,9 @@ class QQ:
 		r = func(url, **kw)
 		self.cookies = r.cookies
 		return r
+
 	def login(self):
-		g=self.fetch(self.checkurl, params={
+		g = self.fetch(self.checkurl, params = {
 			'pt_tea': 1,
 			'uin': self.user,
 			'appid': self.appid,
@@ -37,47 +51,46 @@ class QQ:
 			'js_type': 0,
 			'u1': self.proxyurl,
 		}).text
-		v=re.findall('\'(.*?)\'',g)
-		vcode=v[1]
-		uin=v[2]
-		if v[0]=='1':	# verify code needed
-			vcode=self.getVerifyCode(vcode)
-		g=self.fetch(self.loginurl,params={
-			'u':self.user,
-			'p':self.pwdencode(vcode, uin, self.pwd),
-			'verifycode':vcode,
-			'webqq_type':10,
-			'remember_uin':1,
-			'login2qq':1,
-			'aid':self.appid,
-			'u1':self.proxyurl,
-			'h':1,
-			'ptredirect':0,
-			'ptlang':2052,
-			'daid':164,
-			'from_ui':1,
-			'fp':'loginerroralert',
-			'action':self.action,
-			'mibao_css':'m_webqq',
-			't':1,
-			'g':1,
-			'js_type':0,
-			'js_ver':10120,
-			'pt_randsalt':0,
-			'pt_vcode_v1':0,
-			'pt_verifysession_v1':self.cookies['ptvfsession'],
+		v = re.findall('\'(.*?)\'', g)
+		vcode = v[1]
+		uin = v[2]
+		if v[0] == '1':	# verify code needed
+			vcode = self.getVerifyCode(vcode)
+		g = self.fetch(self.loginurl, params = {
+			'u': self.user,
+			'p': self.pwdencode(vcode, uin, self.pwd),
+			'verifycode': vcode,
+			'webqq_type': 10,
+			'remember_uin': 1,
+			'login2qq': 1,
+			'aid': self.appid,
+			'u1': self.proxyurl,
+			'h': 1,
+			'ptredirect': 0,
+			'ptlang': 2052,
+			'daid': 164,
+			'from_ui': 1,
+			'fp': 'loginerroralert',
+			'action': self.action,
+			'mibao_css': 'm_webqq',
+			't': 1,
+			'g': 1,
+			'js_type': 0,
+			'js_ver': 10120,
+			'pt_randsalt': 0,
+			'pt_vcode_v1': 0,
+			'pt_verifysession_v1': self.cookies['ptvfsession'],
 		}).text
-		r=re.findall('\'(.*?)\'',g)
-		if r[0]!='0': raise LogInError(r[4])
-		self.nick=r[5]
+		r = re.findall('\'(.*?)\'', g)
+		if r[0] != '0':
+			raise LogInError(r[4])
+		self.nick = r[5]
+
 	def pwdencode(self, vcode, uin, pwd):
-		# uin is the bytes of QQ number stored in 8B
-		salt=uin.replace(r'\x','')
-		if isinstance(pwd,str):
-			h1=hashlib.md5(pwd.encode()).digest()
-		else:
-			h1=pwd
-		s2=hashlib.md5(h1+bytes.fromhex(salt)).hexdigest().upper()
+		# uin is the bytes of QQ number stored in unsigned long (8 bytes)
+		salt = uin.replace(r'\x', '')
+		h1 = hashlib.md5(pwd.encode()).digest()
+		s2 = hashlib.md5(h1 + bytes.fromhex(salt)).hexdigest().upper()
 		pub=rsa.PublicKey(int(
 			'F20CE00BAE5361F8FA3AE9CEFA495362'
 			'FF7DA1BA628F64A347F0A8C012BF0B25'
@@ -89,34 +102,36 @@ class QQ:
 			'69A453F27DFDDF83C016D928B3CBF4C7',
 			16
 		), 3)
-		rsaH1=binascii.b2a_hex(rsa.encrypt(h1,pub)).decode()
-		rsaH1Len=hex(len(rsaH1)//2)[2:]
-		hexVcode=binascii.b2a_hex(vcode.upper().encode()).decode()
-		vcodeLen=hex(len(hexVcode)//2)[2:]
-		l=len(vcodeLen)
-		if l<4:
-			vcodeLen='0'*(4-l)+vcodeLen
-		l=len(rsaH1Len)
-		if l<4:
-			rsaH1Len='0'*(4-l)+rsaH1Len
-		pwd1=rsaH1Len+rsaH1+salt+vcodeLen+hexVcode
-		saltPwd=base64.b64encode(tea.encrypt(bytes.fromhex(pwd1),bytes.fromhex(s2))).decode().replace('/','-').replace('+','*').replace('=','_')
+		rsaH1 = binascii.b2a_hex(rsa.encrypt(h1, pub)).decode()
+		rsaH1Len = hex(len(rsaH1) // 2)[2:]
+		hexVcode = binascii.b2a_hex(vcode.upper().encode()).decode()
+		vcodeLen = hex(len(hexVcode) // 2)[2:]
+		l = len(vcodeLen)
+		if l < 4:
+			vcodeLen = '0' * (4 - l) + vcodeLen
+		l = len(rsaH1Len)
+		if l < 4:
+			rsaH1Len = '0' * (4 - l) + rsaH1Len
+		pwd1 = rsaH1Len + rsaH1 + salt + vcodeLen + hexVcode
+		saltPwd = base64.b64encode(
+			tea.encrypt(bytes.fromhex(pwd1), bytes.fromhex(s2))
+		).decode().replace('/', '-').replace('+', '*').replace('=', '_')
 		return saltPwd
-	def getVerifyCode(self,vcode):
-		r=self.fetch(self.imgurl,
-				params={'r':0,'appid':self.appid,
-					'uin':self.user,'vc_type':vcode})
-		tmp=tempfile.mkstemp(suffix='.jpg')
-		os.write(tmp[0],r.raw)
+
+	def getVerifyCode(self, vcode):
+		r = self.fetch(self.imgurl, params = {
+			'r':0,
+			'appid':self.appid,
+			'uin':self.user,
+			'vc_type':vcode,
+		})
+		tmp = tempfile.mkstemp(suffix = '.jpg')
+		os.write(tmp[0], r.raw)
 		os.close(tmp[0])
 		os.startfile(tmp[1])
-		vcode=input('Verify code: ')
+		vcode = input('Verify code: ')
 		os.remove(tmp[1])
 		return vcode
-	def sayHi(self):
-		print('Hi, %s!' % getattr(self,'nick') or self.user)
 
-if __name__=='__main__':
-	q=QQ('12345678','password')
-	q.login()
-	q.sayHi()
+	def sayHi(self):
+		print('Hi, %s!' % getattr(self, 'nick') or self.user)
