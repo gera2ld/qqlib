01d77d991198b42ff7e51d0ddb77f4f9c0790fb9
Gerald <gera2ld@163.com>
2015-04-20 10:53:29 +0800

    added setup.py
    Signed-off-by: Gerald <gera2ld@163.com>

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-pyQQLib
-=====
+QQLib for Python
+===
 
 基于webqq的Python版QQ登录库。
 
-示例
-----
+安装
+---
+``` sh
+$ python3 setup.py install
+```
+或直接复制`qqlib`到合适的位置。
+
+使用方法
+---
 ``` python
 import qqlib
 qq=qqlib.QQ(12345678,'password')
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!python
+# coding=utf-8
+from distutils.core import setup
+
+setup(
+	name='qqlib',
+	version='1.0',
+	packages=['qqlib'],
+	author='Gerald',
+	author_email='gera2ld@163.com',
+	description='QQ lib for Python 3+.',
+	url='http://git.oschina.net/gerald/qqlib',
+)
