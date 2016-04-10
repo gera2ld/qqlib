b4a788814f6ad1bac45f41b562f94aadf1e6813d
Gerald <gera2ld@163.com>
2016-04-10 19:19:55 +0800

    Support installation via pip

--- README.md
+++ README.md
@@ -5,16 +5,9 @@ Python版QQ登录库，兼容Python2.x和Python3.x。
 
 安装
 ---
-1. 安装依赖：
-  ``` sh
-  $ pip install -r requirements.txt
-  ```
-
-1. 安装qqlib：
-  ``` sh
-  $ python setup.py install
-  ```
-  或直接复制`qqlib`到合适的位置。
+``` sh
+$ pip install git+https://github.com/gera2ld/qqlib.git
+```
 
 使用方法
 ---
--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 #!python
 # coding=utf-8
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-	name='qqlib',
-	version='1.0',
-	packages=['qqlib'],
-	author='Gerald',
-	author_email='i@gerald.top',
-	description='QQ lib for Python.',
-	url='https://github.com/gera2ld/qqlib',
+    name = 'qqlib',
+    version = '1.0',
+    packages = find_packages(exclude=['test']),
+    install_requires = [
+        'rsa',
+        'requests',
+    ],
+    author = 'Gerald',
+    author_email = 'i@gerald.top',
+    description = 'QQ lib for Python.',
+    url = 'https://github.com/gera2ld/qqlib',
 )
